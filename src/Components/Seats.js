d7c4389a96de9aa4c9a746185e3086187744bb02
acfernanda <acfernanda98@gmail.com>
2022-03-23 22:08:55 -0300

    render seats

--- src/Components/Seats.js
+++ src/Components/Seats.js
@@ -5,11 +5,40 @@ import { useParams } from "react-router-dom";
 
 export default function Seats() {
   return (
-    <main className="seats">
+    <main className="select-seats">
       <p className="subtitle">Selecione o(s) assento(s)</p>
-      <RenderSeats />
+      <div className="seats">
+        <RenderSeats />
+      </div>
     </main>
   );
 }
 
-function RenderSeats() {}
+function RenderSeats() {
+  const [seats, setSeats] = useState([]);
+  const { idSessao } = useParams();
+
+  useEffect(() => {
+    const promise = axios.get(
+      `https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${idSessao}/seats`
+    );
+
+    promise.then((response) => {
+      setSeats(response.data.seats);
+    });
+  }, []);
+
+  return (
+    <>
+      {seats.map(({ id, name, isAvailable }) => (
+        <div className="seat" key={id}>
+          {isAvailable ? (
+            <div className="available">{name}</div>
+          ) : (
+            <div className="unavailable">{name}</div>
+          )}
+        </div>
+      ))}
+    </>
+  );
+}
